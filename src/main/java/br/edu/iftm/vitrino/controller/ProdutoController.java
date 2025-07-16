87bba45627606919b428d0398ed9b3091267f66f
LilKtss <lucas.barros.palmeira15@gmail.com>
2025-07-15 21:44:27 -0300

    Update ProdutoController.java

--- src/main/java/br/edu/iftm/vitrino/controller/ProdutoController.java
+++ src/main/java/br/edu/iftm/vitrino/controller/ProdutoController.java
@@ -3,7 +3,7 @@ package br.edu.iftm.vitrino.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus; // Para usar HttpStatus.CREATED
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity; 
 import org.springframework.web.bind.annotation.*;
 
 import br.edu.iftm.vitrino.entity.Produto;
